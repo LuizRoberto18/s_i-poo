e02f08d9311cba6433bb523a3cd1284270fc326d
LUIZ ROBERTO TORRES DOS SANTOS <2023011985@MACEIO.IFAL>
2025-10-17 21:47:18 -0300

    adicionando aluno

---
+++ aula20251017/Aluno.java
@@ -0,0 +1,7 @@
+public class Aluno extends Pessoa{
+    
+    @Override
+    public void falar() {
+        System.out.println("falo como  aluno");
+    }
+}
--- aula20251017/Pessoa.java
+++ aula20251017/Pessoa.java
@@ -4,4 +4,9 @@ public abstract class Pessoa{
     private String cpf;
     private int idadee;
     
+    public void falar(){
+        System.out.println(
+            "falando"
+        );
+    }
 }
